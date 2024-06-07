ecfc30c1ffc1f7b6fa7eedaac1f57aaee85a01bd
agroce <agroce@gmail.com>
2024-06-07 08:27:33 -0700

    proper funciton name split

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys, os
 
 setup(
     name='muttfuzz',
-    version='0.9.19',
+    version='0.9.20',
     description='Fuzzing with mutants',
     long_description_content_type="text/markdown",
     long_description=open('README.md').read(),
