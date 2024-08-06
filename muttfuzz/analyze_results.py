12c5ec6eda4d424351125eee3c10eebf313a21c0
agroce <agroce@gmail.com>
2024-08-06 10:28:12 -0700

    add analysis support

---
+++ muttfuzz/analyze_results.py
@@ -0,0 +1,36 @@
+import csv
+import sys
+
+import scipy
+import scipy.stats
+
+files = sys.argv[1:]
+
+roots = {}
+
+for f in files:
+    root = f.split(".")[0]
+    if root not in roots:
+        roots[root] = []
+    with open(f) as fr:
+        rows = csv.reader(fr)
+        for row in rows:
+            roots[root].append(row)
+
+for r in roots.keys():
+    d_t = list(map(lambda x:x[1], roots[r]))
+    d_m = list(map(lambda x:x[2], roots[r]))
+    score = len(list(filter(lambda x: x != 0, d_m))) / len(list(d_m)) 
+    print(r, "MEAN:", round(scipy.mean(d_t), 2), "MEDIAN:", round(scipy.median(d_t), 2), "RANGE: [" + round(min(d_t), 2), "-", round(max(d_t), 2), "]")
+    print(r, "MUTATION SCORE:", round(score, 2))
+    print()
+    
+for r1 in roots.keys():
+    d_t_1 = list(map(lambda x:x[1], roots[r1]))
+    for r2 in roots.keys():
+        if (r1 < r2):
+            d_t_2 = list(map(lambda x:x[1], roots[r2]))
+            try:
+                print("Mann-Whitney U:", scipy.stats.mannwhitneyu(d_t_1, d_t_2))
+            except:
+                pass
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='muttfuzz',
-    version='1.2.11',
+    version='1.2.12',
     description='Fuzzing with mutants',
     long_description_content_type="text/markdown",
     long_description=open('README.md').read(),
@@ -16,6 +16,7 @@ setup(
     muttfuzz = muttfuzz.fuzz:main
     apply_mutant = muttfuzz.apply_mutant:main
     libfuzzer_prune = muttfuzz.libfuzzer_prune:main
+    analyze_results = muttfuzz.analyze_results:main
     """,
     keywords='fuzzing mutation',
     classifiers=[
