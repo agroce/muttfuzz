43aa76cf3f7fe93ff9e0ab11f9446c2ed477ffb3
agroce <agroce@gmail.com>
2024-08-25 07:15:27 -0700

    tests

---
+++ test/test_basic.py
@@ -0,0 +1,22 @@
+import subprocess
+
+import pytest
+
+def test_record_replay():
+    r = subprocess.call(["gcc -o test/toy toy.c"], shell=True)
+    assert(r == 0)
+
+    with open("out1.txt", 'w') as f:
+        r = subprocess.call(["mkdir mutants; muttfuzz "./toy" toy --score --avoid_repeats --stop_on_repeat --repeat_retries 2000 --save_results analysis.csv --save_mutants mutants"], shell=True, stdout=f, stderr=f)
+    assert(r == 0)        
+    with open("out1.txt", 'r') as f:
+        contents = f.read()
+    assert("FINAL MUTATION SCORE OVER 14 EXECUTED MUTANTS: 57.14%" in contents)
+
+    with open("out2.txt", 'w') as f:
+        r = subprocess.call(["muttfuzz "./toy" toy --score --avoid_repeats --stop_on_repeat --save_results s_analysis.csv --use_saved_mutants mutants"], shell=True, stdout=f, stderr=f)
+    assert(r == 0)
+    with open("out2.txt", 'r') as f:
+        contents = f.read()
+    assert("FINAL MUTATION SCORE OVER 14 EXECUTED MUTANTS: 57.14%" in contents)
+    
